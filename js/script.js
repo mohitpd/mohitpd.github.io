ed88383f0cf527320a66143a093ab3b6dcd2c015
mohitpd <mohit.prasad@roadzen.io>
2023-05-02 09:02:30 +0530

    Scripts Updated

--- js/script.js
+++ js/script.js
@@ -1,29 +1,63 @@
 /* Typing Animation */
-var typed = new Typed('.typing', {
-    strings: ["","Web Developer", "Full Stack Developer", "MERN Stack Developer", "React Developer"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop:true
-})
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer",
+    "Full Stack Developer",
+    "MERN Stack Developer",
+    "React Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
 
 /* Aside Animation */
 const nav = document.querySelector(".nav"),
-    navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length,
-    allSection = document.querySelectorAll(".section")
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
 
-    for(let i =0;i<totalNavList;i++){
-        const a = navList[i].querySelector("a")
-        a.addEventListener("click", function(){
-            for(let j = 0;j<totalNavList;j++){
-                navList[j].querySelector("a").classList.remove("active")
-            }
-            this.classList.add("active");
-            // showSection(this);
-        })
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
     }
-    // function showSection(element) {
-    //     console.log(element.getAttribute("href"))
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
+    }
+  });
+}
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href");
+  document.querySelector(target).classList.add("active");
+}
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
 
-        
-    // }
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  navTogglerBtn.querySelector("i").classList.toggle("fa-bars");
+  navTogglerBtn.querySelector("i").classList.toggle("fa-xmark");
+
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
